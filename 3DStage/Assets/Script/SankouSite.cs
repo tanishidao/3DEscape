7ab0c96d916d43f606e9ab67837cb12b8998306f
tanishidao <tanitaniumauma1003@gmail.com>
2021-06-27 13:15:04 +0900

    zombi and sannkousite ireta

---
+++ 3DStage/Assets/Script/SankouSite.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SankouSite : MonoBehaviour
+{
+    ///後ろ！「https://www.ame-name.com/archives/3055」
+    ///キャラを目的地へ移動「https://gametukurikata.com/program/enemymovedestination」
+    ///動き回る「https://gametukurikata.com/program/movearounddestination」
+    ///追いかける「https://gametukurikata.com/program/enemychasechara」
+
+}
