9524962d16ab73d6b354fbab52dc6f89120a9910
tanishidao <tanitaniumauma1003@gmail.com>
2021-07-17 22:10:16 +0900

    できたこと つかむスクリプト見つけた

--- 3DStage/Assets/Script/SankouSite.cs
+++ 3DStage/Assets/Script/SankouSite.cs
@@ -8,5 +8,5 @@ public class SankouSite : MonoBehaviour
     ///キャラを目的地へ移動「https://gametukurikata.com/program/enemymovedestination」
     ///動き回る「https://gametukurikata.com/program/movearounddestination」
     ///追いかける「https://gametukurikata.com/program/enemychasechara」
-
+    ///つかむ「https://www.ame-name.com/archives/782」
 }
